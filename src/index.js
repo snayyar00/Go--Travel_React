6e99fbea6a4d782cf5203800640e077185269d1e
Sidharth <sidharth15nayyar@gmail.com>
2020-08-24 12:59:35 -0700

    done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import myImage from './videos/video-1.mp4';
+import image1 from './img-1.png';
+import video1 from './video-1.mp4';
+
 ReactDOM.render(<App />, document.getElementById('root'));
