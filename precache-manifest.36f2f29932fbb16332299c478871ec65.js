6ebdd4246d496f7f76d71c7204148e88161c7c0c
Sidharth <sidharth15nayyar@gmail.com>
2020-08-23 23:45:42 -0700

    Updates

---
+++ precache-manifest.36f2f29932fbb16332299c478871ec65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5970d3c6bd1dbac93cf35e13cfe5a32e",
+    "url": "/Go--Travel_React/index.html"
+  },
+  {
+    "revision": "e1592a0525cffa8a2f3c",
+    "url": "/Go--Travel_React/static/css/main.583a2bf0.chunk.css"
+  },
+  {
+    "revision": "99fb04a914555e931017",
+    "url": "/Go--Travel_React/static/js/2.2efb28fc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Go--Travel_React/static/js/2.2efb28fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1592a0525cffa8a2f3c",
+    "url": "/Go--Travel_React/static/js/main.689db997.chunk.js"
+  },
+  {
+    "revision": "137cef1ff166f1f20716",
+    "url": "/Go--Travel_React/static/js/runtime-main.97540018.js"
+  }
+]);
